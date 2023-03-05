3b4d995974aeef78a23e7d104066195e7fbeeb2f
Mariia <shvetsmariia@yahoo.com>
2023-03-04 20:17:35 -0600

    updated header & footer

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function showDiv () {
+    document.getElementById('menuPopup').style.display = "block"; 
+}
+
+function closeMenu () {
+    document.getElementById('menuPopup').style.display = "none"; 
+}
